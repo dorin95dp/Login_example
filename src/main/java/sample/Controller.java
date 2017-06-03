485e30b142e87c63a0e42f671ed30fce257d5ac9
dorin95dp <dorin.popa1995@gmail.com>
2017-06-03 09:44:11 +0300

    Create ex. of actions on button, label, textField

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,10 +1,26 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
-    public void printNumber(ActionEvent event) {
-        System.out.println(21);
+    @FXML
+    private Label label1;
+    @FXML
+    private Label label2;
+    @FXML
+    private TextField textField1;
+
+    public void printSomething() {
+        System.out.println("method print something works");
+        label1.setText("Jora");
+        textField1.setText("Grisha");
+    }
+
+    public void setLabel2() {
+        label2.setText(textField1.getText());
     }
 }
